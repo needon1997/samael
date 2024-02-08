a0d295eb8259caa35a3129edc84e40d63227e17e
weiy <weiy3458@gmail.com>
2024-02-08 00:45:04 -0800

    change binding

--- src/xmlsec/backend.rs
+++ src/xmlsec/backend.rs
@@ -11,9 +11,10 @@ mod backend {
         xmlSecOpenSSLAppInit as xmlSecCryptoAppInit,
         xmlSecOpenSSLAppKeyCertLoad as xmlSecCryptoAppKeyCertLoad,
         xmlSecOpenSSLAppKeyCertLoadMemory as xmlSecCryptoAppKeyCertLoadMemory,
-        xmlSecOpenSSLAppKeyLoad as xmlSecCryptoAppKeyLoad,
+        xmlSecOpenSSLAppKeyLoadEx as xmlSecCryptoAppKeyLoad,
         xmlSecOpenSSLAppKeyLoadMemory as xmlSecCryptoAppKeyLoadMemory,
-        xmlSecOpenSSLAppShutdown as xmlSecCryptoAppShutdown, xmlSecOpenSSLInit as xmlSecCryptoInit,
+        xmlSecOpenSSLAppShutdown as xmlSecCryptoAppShutdown,
+        xmlSecOpenSSLInit as xmlSecCryptoInit,
         xmlSecOpenSSLShutdown as xmlSecCryptoShutdown,
     };
 }
