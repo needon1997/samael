4a436cbda593c7f53b33f10980789505381e38aa
Yidong <weiy3458@gmail.com>
2024-04-28 16:48:46 -0700

    support macos backendw

--- src/xmlsec/backend.rs
+++ src/xmlsec/backend.rs
@@ -6,6 +6,7 @@
 use crate::bindings as backend;
 
 #[cfg(xmlsec_static)]
+#[cfg(target_os = "mac_os")]
 mod backend {
     pub use crate::bindings::{
         xmlSecOpenSSLAppInit as xmlSecCryptoAppInit,
@@ -13,8 +14,21 @@ mod backend {
         xmlSecOpenSSLAppKeyCertLoadMemory as xmlSecCryptoAppKeyCertLoadMemory,
         xmlSecOpenSSLAppKeyLoadEx as xmlSecCryptoAppKeyLoad,
         xmlSecOpenSSLAppKeyLoadMemory as xmlSecCryptoAppKeyLoadMemory,
-        xmlSecOpenSSLAppShutdown as xmlSecCryptoAppShutdown,
-        xmlSecOpenSSLInit as xmlSecCryptoInit,
+        xmlSecOpenSSLAppShutdown as xmlSecCryptoAppShutdown, xmlSecOpenSSLInit as xmlSecCryptoInit,
+        xmlSecOpenSSLShutdown as xmlSecCryptoShutdown,
+    };
+}
+
+#[cfg(xmlsec_static)]
+#[cfg(target_os = "linux")]
+mod backend {
+    pub use crate::bindings::{
+        xmlSecOpenSSLAppInit as xmlSecCryptoAppInit,
+        xmlSecOpenSSLAppKeyCertLoad as xmlSecCryptoAppKeyCertLoad,
+        xmlSecOpenSSLAppKeyCertLoadMemory as xmlSecCryptoAppKeyCertLoadMemory,
+        xmlSecOpenSSLAppKeyLoad as xmlSecCryptoAppKeyLoad,
+        xmlSecOpenSSLAppKeyLoadMemory as xmlSecCryptoAppKeyLoadMemory,
+        xmlSecOpenSSLAppShutdown as xmlSecCryptoAppShutdown, xmlSecOpenSSLInit as xmlSecCryptoInit,
         xmlSecOpenSSLShutdown as xmlSecCryptoShutdown,
     };
 }
